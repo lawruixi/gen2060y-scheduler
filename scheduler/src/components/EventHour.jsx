13949c92532ca3079b02ebace8ed21813ca4e3a7
Evelyne Law <lawruixi@gmail.com>
2025-06-20 10:46:09 +0800

    Added Tooltips for the EventHours.

--- scheduler/src/components/EventHour.jsx
+++ scheduler/src/components/EventHour.jsx
@@ -1,3 +1,21 @@
+import { Tooltip } from 'react-tooltip';
+
+function hourOfDayToString(hour_of_day) {
+    // Jank String manip; could have used a switch statement, but that's a bit too high-effort for this proof-of-concept.
+    if (hour_of_day == 0) {
+        return "12:00 AM";
+    }
+    if (hour_of_day < 12) {
+        return `${hour_of_day}:00 AM`;
+    }
+    if (hour_of_day == 12) {
+        return `${hour_of_day}:00 PM`;
+    }
+    if (hour_of_day < 24) {
+        return `${hour_of_day-12}:00 PM`;
+    }
+}
+
 function EventHour({schedules, currentScheduleUsername, currentScheduleDate, hour}) {
     const schedule_filtered_by_username = schedules[currentScheduleUsername] ?? [];
 
@@ -13,11 +31,35 @@ function EventHour({schedules, currentScheduleUsername, currentScheduleDate, hou
         schedule_date.getHours()
     ));
 
-    if (schedule_mapped_hours.includes(hour)) {
-        return <td className="hour-td active"></td>
+    let numberOfAvailableUsers = 0;
+    for (const username in schedules) {
+        if (schedules[username].some((date) => {
+            return date.getDate() == currentScheduleDate.getDate() &&
+                date.getMonth() == currentScheduleDate.getMonth() &&
+                date.getFullYear() == currentScheduleDate.getFullYear() &&
+                date.getHours() == hour
+        })) {
+            numberOfAvailableUsers += 1;
+        }
     }
 
-    return <td className="hour-td"></td>
+    // Tooltips
+    const tooltip_string = 
+        `${currentScheduleDate.getFullYear()}-${currentScheduleDate.getMonth()}-${currentScheduleDate.getDate()}` + '&nbsp;' +
+        `${hourOfDayToString(hour)}` + '<br />' +
+        `${numberOfAvailableUsers} participant(s) available.`
+
+    let td_classes = "hour-td";
+
+    if (schedule_mapped_hours.includes(hour)) {
+        td_classes += " active";
+    }
+
+    return <>
+        <td className={td_classes} data-tooltip-id={`tooltip-${hour}`} data-tooltip-html={tooltip_string}>
+            <Tooltip id={`tooltip-${hour}`}/>
+        </td>
+    </>
 }
 
 export default EventHour;
